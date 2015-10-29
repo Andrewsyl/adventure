d011827fb6eca84229262d162f20415b730dfd6f
Andrewsyl <andrew.costello@ucdconnect.ie>
2015-10-29 15:19:43 +0000

    added comment

--- adventure.py
+++ adventure.py
@@ -1,4 +1,4 @@
-from data import locations
+from data import locations, description, items, map
 
 directions = {
     'west': (-1,0),
@@ -6,25 +6,46 @@ directions = {
     'north': (0,-1),
     'south':(0,1),
 }
+#YES = {"You got the %s" % items}
 
 position = (0,0)
 
 while True:
     location = locations[position]
+    descriptions = description[position]
+    stuff = items[position]
+    mapo = map[position]
     print 'you are at the %s' % location
+    print '%s' % descriptions
+    print '%s' % mapo
+
+    Choice = raw_input('You find a %s. Do you pick it up? YES/NO\n' % stuff)
+    if Choice == 'yes':
+        print "You picked up the %s" % stuff
+    else:
+        print "You leave the %s and keep going" % stuff
+
+
+
 
     valid_directions = {}
     for k, v in directions.iteritems():
         possible_position = (position[0] + v[0], position[1] + v[1])
         possible_location = locations.get(possible_position)
+        possible_description = description.get(possible_position)
+        mapo = map.get(possible_position)
+
         if possible_location:
-            print 'to the %s is a %s' % (k, possible_location)
+            print 'to the %s is a %s' % (k, possible_location,)
             valid_directions[k] = possible_position
 
-    direction = raw_input('which direction do you want to go?')
-#comment here
+
+    direction = raw_input('which direction do you want to go?\n')
     new_position = valid_directions.get(direction)
     if new_position:
         position = new_position
     else:
-        print "ZOMBIES!!!Turn Back!"
+        print "You walked into a fence"
+
+
+
